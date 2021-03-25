68663202cd83c107a0f8c52faea4ead60a35bd84
PaoloGiraudi <58827439+PaoloGiraudi@users.noreply.github.com>
2021-03-25 21:31:48 +0100

    test api with dummy images added

--- src/Context.js
+++ src/Context.js
@@ -1,9 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+// import { MusicBrainzApi } from "musicbrainz-api";
+// const mbApi = new MusicBrainzApi();
+// const apiUrl = "http://musicbrainz.org/ws/2/artist/?query=daft-punk&fmt=json";
 
 const Context = React.createContext();
 
 function ContextProvider({ children }) {
   const [allItems, setAllItems] = useState([]);
+  const apiUrl =
+    "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json";
+
+  useEffect(async () => {
+    // const getArtist = async () => {
+    //   const artist = await mbApi.getArtist(
+    //     "056e4f3e-d505-4dad-8ec1-d04f521cbb56",
+    //     ["releases", "recordings", "url-rels"]
+    //   );
+    //   console.log(artist);
+    // };
+    // getArtist();
+
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => setAllItems(data));
+  }, []);
+
   return <Context.Provider value={{ allItems }}>{children}</Context.Provider>;
 }
 
