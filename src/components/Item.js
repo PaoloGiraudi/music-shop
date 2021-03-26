28e79c3cddb87a8da0b8a92ae2314646bbefa259
PaoloGiraudi <58827439+PaoloGiraudi@users.noreply.github.com>
2021-03-26 12:28:07 +0100

    added icons on hover

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  HiOutlineHeart,
+  HiHeart,
+  HiOutlineShoppingCart,
+  HiShoppingCart,
+} from "react-icons/hi";
 
 export default function Item({ item }) {
+  const [hovered, setHovered] = useState(false);
+
+  const heartIcon = hovered && <HiOutlineHeart className="favorite" />;
+  const cartIcon = hovered && <HiOutlineShoppingCart className="cart" />;
+
   return (
-    <div>
-      <img src={item.url} alt="" />
+    <div
+      className="image-container"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <img style={{ maxWidth: "100%" }} src={item.url} alt="" />
+      {cartIcon}
+      {heartIcon}
     </div>
   );
 }
