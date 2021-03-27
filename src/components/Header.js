ffa536f76c72f70585bae3deb0f0f90b5650dce7
PaoloGiraudi <58827439+PaoloGiraudi@users.noreply.github.com>
2021-03-27 11:54:55 +0100

    cart icon toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { FiShoppingCart } from "react-icons/fi";
+import { HiOutlineShoppingCart, HiShoppingCart } from "react-icons/hi";
+import { Context } from "../Context";
 
 export default function Header() {
+  const { cartItems } = useContext(Context);
+
+  const cartIcon =
+    cartItems.length > 0 ? <HiShoppingCart /> : <HiOutlineShoppingCart />;
+
   return (
     <header>
       <Link to="/">
         <h1>Music Shop</h1>
       </Link>
-      <Link to="/cart">
-        <FiShoppingCart />
-      </Link>
+      <Link to="/cart">{cartIcon}</Link>
     </header>
   );
 }
