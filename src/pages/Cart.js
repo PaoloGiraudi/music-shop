c6b79b1e338e7367ecfa86da79e991f0d0efe151
PaoloGiraudi <58827439+PaoloGiraudi@users.noreply.github.com>
2021-03-27 12:23:37 +0100

    added items to cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,20 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
+import CartItem from "../components/CartItem";
+import { Context } from "../Context";
 
 export default function Cart() {
+  const { cartItems } = useContext(Context);
+  const cartItemElements = cartItems.map((item) => (
+    <CartItem key={item.id} item={item} />
+  ));
   return (
-    <main>
+    <main className="cart-page">
       <h3>Check out</h3>
+      {cartItemElements}
+      <p className="total-cost">Total:</p>
+      <div className="order-button">
+        <button>Place Order</button>
+      </div>
     </main>
   );
 }
