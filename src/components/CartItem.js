f8f01c95fd3cd9a1dd146151439f75556c59f495
PaoloGiraudi <58827439+PaoloGiraudi@users.noreply.github.com>
2021-05-06 15:46:23 +0200

    added album and artist in cart

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -12,6 +12,10 @@ export default function CartItem({ item }) {
         onClick={() => removeFromCart(item.id)}
       />
       <img className="w-36" src={item.url} alt={item.artist} />
+      <div className="ml-4">
+        <h3 className="text-xl mb-1">{item.album}</h3>
+        <h4 className="text-gray-500">{item.artist}</h4>
+      </div>
       <p className="flex-grow text-lg text-right mr-4">$5.99</p>
     </div>
   );
